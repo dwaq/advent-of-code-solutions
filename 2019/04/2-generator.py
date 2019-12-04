244e4bbc5c540c37b8ba71f3e447a0130e6066f1
Dillon Nichols <ee.tinkerer@gmail.com>
2019-12-04 08:05:56 -0500

    copy first file

---
+++ 2019/04/2-generator.py
@@ -0,0 +1,42 @@
+# RULES:
+# 6 digits (automatically true in this range)
+# within range of puzzle input (done)
+# two adjacent digits are the same (done)
+# left to right, digits never decrease (only increase or stay the same) (done)
+
+total = 0
+
+for n in range(137683,596253+1):
+    # convert to string
+    s = str(n)
+    # split into letters
+    l = list(s)
+    # convert back to ints
+    i = [int(letter) for letter in l]
+    
+    # check for adjacent digits
+    adjacent = False
+    # don't loop through the last one because we're checking next vs current
+    for x, d in enumerate(i[:-1]):
+        # check if current number (d) matches the next number (index + 1)
+        if (d == i[x+1]):
+            #print("adjacent digits:", n)
+            adjacent = True
+            break
+
+    # check that digits increase
+    increase = True
+    # needs to be true to continue
+    if (adjacent == True):
+        # check that digits increase
+        for x, d in enumerate(i[:-1]):
+            # problem if current number (d) is greater than next number (index + 1)
+            if (d > i[x+1]):
+                increase = False
+
+        # checked everything; add to total if still true
+        if (increase == True):
+            #print("\tdigits increase:", n)
+            total+=1
+
+print("total: ", total)
