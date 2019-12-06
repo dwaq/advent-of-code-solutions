4bb89ed5c28090f306b4b681ccdec860e8212260
Dillon Nichols <ee.tinkerer@gmail.com>
2019-12-06 09:34:57 -0500

    generated the tree

--- 2019/06/1-orbit.py
+++ 2019/06/1-orbit.py
@@ -13,43 +13,28 @@ for o in map:
     # split based on orbit
     orbits.append(o.split(')'))
 
-print(orbits)
+#print(orbits)
 
-counts = {}
-'''
-# find root
-for oo in orbits:
-    for o in oo:
-        # if already there, increase count
-        if o in counts:
-            counts[o] += 1
-        # otherwise set to 1
-        else:
-            counts[o] = 1
-
-print(counts)
-'''
-'''
-def stringToInt(orbit):
-    #print(orbit)
-    num = 0
-    for l in orbit:
-        num += ord(l)
-    return num
-'''
-
+# use dictonary to store the tree in a way I can refer to it later
+# https://stackoverflow.com/a/1373185/7564623
 tree = {}
 
 # set root
 tree[orbits[0][0]] = Node(orbits[0][0])
-#root = Node(orbits[0][0])
+# set variable for future use
+root = tree[orbits[0][0]]
+
+# set first child
 tree[orbits[0][1]] = Node(orbits[0][1], parent=tree[orbits[0][0]])
 
 # loop through remaining orbits
-#for o in orbits[1:]:
-#    newone = Node(o[1], o[0])    
+for orbit in orbits[1:]:
+    # set as child
+    tree[orbit[1]] = Node(orbit[1], parent=tree[orbit[0]])
 
 #print(root)
 
 # display full tree
-print(RenderTree(tree[orbits[0][0]]))
+#print(RenderTree(tree[orbits[0][0]]))
+for pre, fill, node in RenderTree(root):
+    print("%s%s" % (pre, node.name))
