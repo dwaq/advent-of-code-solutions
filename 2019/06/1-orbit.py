6ff8ebea75e6e1a225feb8b17e5cd47224ace679
Dillon Nichols <ee.tinkerer@gmail.com>
2019-12-06 11:20:29 -0500

    need to order list to start from root

--- 2019/06/1-orbit.py
+++ 2019/06/1-orbit.py
@@ -3,7 +3,7 @@ from anytree import Node, RenderTree
 # open file
 map = open('map.txt', 'r')
 
-orbits = []
+unorderedOrbits = []
 
 # go through each
 for o in map:
@@ -11,11 +11,30 @@ for o in map:
     o = o.strip('\n')
 
     # split based on orbit
-    orbits.append(o.split(')'))
+    unorderedOrbits.append(o.split(')'))
+
+#print(unorderedOrbits)
+
+# reorder list to start at root
+orbits = []
+
+def orderList(parent):
+    # loop through each
+    for o in unorderedOrbits:
+        # add to list if parent
+        if (o[0] == parent):
+            orbits.append(o)
+            # do the same to that child
+            orderList(o[1])
+
+# start at COM
+orderList('COM')
+
+#print(len(unorderedOrbits), len(orbits))
 
 #print(orbits)
 
-# use dictonary to store the tree in a way I can refer to it later
+# use dictionary to store the tree in a way I can refer to it later
 # https://stackoverflow.com/a/1373185/7564623
 tree = {}
 
@@ -35,8 +54,8 @@ for orbit in orbits[1:]:
 #print(root)
 
 # display full tree
-for pre, fill, node in RenderTree(root):
-    print("%s%s" % (pre, node.name))
+#for pre, fill, node in RenderTree(root):
+#    print("%s%s" % (pre, node.name))
 
 # get the paths of every orbit
 paths = root.descendants
