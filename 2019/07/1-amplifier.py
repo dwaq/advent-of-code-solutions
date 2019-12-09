2ba14cc0ebfac8b7ed0dcb81cad87763bb3311b2
Dillon Nichols <ee.tinkerer@gmail.com>
2019-12-09 08:45:01 -0500

    calculated the highest output signal

--- 2019/07/1-amplifier.py
+++ 2019/07/1-amplifier.py
@@ -1,113 +1,127 @@
-# input to computer
-inputValue = 5
+from itertools import permutations
 
-#intcode = "1, 9, 10, 3, 2, 3, 11, 0, 99, 30, 40, 50"
-#intcode = "1,0,0,0,99"
-#intcode = "2,3,0,3,99"
-#intcode = "2,4,4,5,99,0"
-#intcode = "1,1,1,4,99,5,6,0,99"
-#intcode = "1,0,0,3,1,1,2,3,1,3,4,3,1,5,0,3,2,1,10,19,1,19,6,23,2,23,13,27,1,27,5,31,2,31,10,35,1,9,35,39,1,39,9,43,2,9,43,47,1,5,47,51,2,13,51,55,1,55,9,59,2,6,59,63,1,63,5,67,1,10,67,71,1,71,10,75,2,75,13,79,2,79,13,83,1,5,83,87,1,87,6,91,2,91,13,95,1,5,95,99,1,99,2,103,1,103,6,0,99,2,14,0,0"
-#intcode = "3,0,4,0,99"
-#intcode = "1002,4,3,4,33"
-intcode = "3,225,1,225,6,6,1100,1,238,225,104,0,1102,57,23,224,101,-1311,224,224,4,224,1002,223,8,223,101,6,224,224,1,223,224,223,1102,57,67,225,102,67,150,224,1001,224,-2613,224,4,224,1002,223,8,223,101,5,224,224,1,224,223,223,2,179,213,224,1001,224,-469,224,4,224,102,8,223,223,101,7,224,224,1,223,224,223,1001,188,27,224,101,-119,224,224,4,224,1002,223,8,223,1001,224,7,224,1,223,224,223,1,184,218,224,1001,224,-155,224,4,224,1002,223,8,223,1001,224,7,224,1,224,223,223,1101,21,80,224,1001,224,-101,224,4,224,102,8,223,223,1001,224,1,224,1,224,223,223,1101,67,39,225,1101,89,68,225,101,69,35,224,1001,224,-126,224,4,224,1002,223,8,223,1001,224,1,224,1,224,223,223,1102,7,52,225,1102,18,90,225,1101,65,92,225,1002,153,78,224,101,-6942,224,224,4,224,102,8,223,223,101,6,224,224,1,223,224,223,1101,67,83,225,1102,31,65,225,4,223,99,0,0,0,677,0,0,0,0,0,0,0,0,0,0,0,1105,0,99999,1105,227,247,1105,1,99999,1005,227,99999,1005,0,256,1105,1,99999,1106,227,99999,1106,0,265,1105,1,99999,1006,0,99999,1006,227,274,1105,1,99999,1105,1,280,1105,1,99999,1,225,225,225,1101,294,0,0,105,1,0,1105,1,99999,1106,0,300,1105,1,99999,1,225,225,225,1101,314,0,0,106,0,0,1105,1,99999,1007,226,226,224,102,2,223,223,1005,224,329,1001,223,1,223,108,677,226,224,1002,223,2,223,1005,224,344,1001,223,1,223,1007,677,677,224,1002,223,2,223,1005,224,359,1001,223,1,223,1107,677,226,224,102,2,223,223,1006,224,374,1001,223,1,223,8,226,677,224,1002,223,2,223,1006,224,389,101,1,223,223,8,677,677,224,102,2,223,223,1006,224,404,1001,223,1,223,1008,226,226,224,102,2,223,223,1006,224,419,1001,223,1,223,107,677,226,224,102,2,223,223,1006,224,434,101,1,223,223,7,226,226,224,1002,223,2,223,1005,224,449,1001,223,1,223,1107,226,226,224,1002,223,2,223,1006,224,464,1001,223,1,223,1107,226,677,224,1002,223,2,223,1005,224,479,1001,223,1,223,8,677,226,224,1002,223,2,223,1006,224,494,1001,223,1,223,1108,226,677,224,1002,223,2,223,1006,224,509,101,1,223,223,1008,677,677,224,1002,223,2,223,1006,224,524,1001,223,1,223,1008,677,226,224,102,2,223,223,1006,224,539,1001,223,1,223,1108,677,677,224,102,2,223,223,1005,224,554,101,1,223,223,108,677,677,224,102,2,223,223,1006,224,569,101,1,223,223,1108,677,226,224,102,2,223,223,1005,224,584,1001,223,1,223,108,226,226,224,1002,223,2,223,1005,224,599,1001,223,1,223,1007,226,677,224,102,2,223,223,1005,224,614,1001,223,1,223,7,226,677,224,102,2,223,223,1006,224,629,1001,223,1,223,107,226,226,224,102,2,223,223,1005,224,644,101,1,223,223,7,677,226,224,102,2,223,223,1005,224,659,101,1,223,223,107,677,677,224,1002,223,2,223,1005,224,674,1001,223,1,223,4,223,99,226"
-#intcode, inputValue = "3,9,8,9,10,9,4,9,99,-1,8", 8
-#intcode, inputValue = "3,9,7,9,10,9,4,9,99,-1,8", 7
-#intcode, inputValue = "3,3,1108,-1,8,3,4,3,99", 8
-#intcode, inputValue = "3,3,1107,-1,8,3,4,3,99", 7
-#intcode, inputValue = "3,12,6,12,15,1,13,14,13,4,13,99,-1,0,1,9", 1
-#intcode, inputValue = "3,3,1105,-1,9,1101,0,0,12,4,12,99,1", 0
-#intcode, inputValue = "3,21,1008,21,8,20,1005,20,22,107,8,21,20,1006,20,31,1106,0,36,98,0,0,1002,21,125,20,4,20,1105,1,46,104,999,1105,1,46,1101,1000,1,20,4,20,1105,1,46,98,99", 8
+# storage for the 5x stages
+# index 0 of array is the phase setting (0-4, each used once)
+# index 1 of array is the previous amplifier's output signal
+storage = [ [0,0],
+            [1,0],
+            [2,0],
+            [3,0],
+            [4,0],
+            [0,0]   # only used for final output
+        ]
 
-instructions = intcode.split(",")
+# store the highest signal that is seen
+highestSignal = 0
 
-# convert all numbers to int
-instructions = [int(x) for x in instructions]
-
-# current instruction position
-position = 0
-
-#output to user
-outputValue = 0
-
-# end at 99
-while(instructions[position] != 99):
-    # current op code (lowest 2 digits)
-    i = instructions[position]%100
+# get every permutation of the phase setting
+possibilities = set(permutations(range(5), 5))
+for possibility in possibilities:
+    # fill the storage array with that permutation
+    for i, p in enumerate(possibility):
+        storage[i] = [p, 0]
     
-    # get mode of each parameter
-    # // does floor division (no floats)
-    # %10 only gets that digit
-    mode1 = (instructions[position]//  100)%10
-    mode2 = (instructions[position]// 1000)%10
-    mode3 = (instructions[position]//10000)%10
+    #calculate the output from that phase setting
+    # there's 5x stages
+    for amplifierStage in range(5):
+        # amplifier instructions (reset for each loop)
+        intcode = "3,8,1001,8,10,8,105,1,0,0,21,34,43,60,81,94,175,256,337,418,99999,3,9,101,2,9,9,102,4,9,9,4,9,99,3,9,102,2,9,9,4,9,99,3,9,102,4,9,9,1001,9,4,9,102,3,9,9,4,9,99,3,9,102,4,9,9,1001,9,2,9,1002,9,3,9,101,4,9,9,4,9,99,3,9,1001,9,4,9,102,2,9,9,4,9,99,3,9,102,2,9,9,4,9,3,9,1001,9,2,9,4,9,3,9,102,2,9,9,4,9,3,9,101,1,9,9,4,9,3,9,1002,9,2,9,4,9,3,9,1001,9,2,9,4,9,3,9,101,2,9,9,4,9,3,9,101,1,9,9,4,9,3,9,102,2,9,9,4,9,3,9,1002,9,2,9,4,9,99,3,9,101,2,9,9,4,9,3,9,1001,9,1,9,4,9,3,9,101,1,9,9,4,9,3,9,102,2,9,9,4,9,3,9,1002,9,2,9,4,9,3,9,1001,9,1,9,4,9,3,9,1001,9,2,9,4,9,3,9,101,1,9,9,4,9,3,9,1001,9,1,9,4,9,3,9,1001,9,2,9,4,9,99,3,9,1002,9,2,9,4,9,3,9,102,2,9,9,4,9,3,9,1002,9,2,9,4,9,3,9,102,2,9,9,4,9,3,9,1001,9,1,9,4,9,3,9,1002,9,2,9,4,9,3,9,1001,9,2,9,4,9,3,9,101,1,9,9,4,9,3,9,101,2,9,9,4,9,3,9,1001,9,2,9,4,9,99,3,9,101,2,9,9,4,9,3,9,101,1,9,9,4,9,3,9,101,1,9,9,4,9,3,9,101,1,9,9,4,9,3,9,1001,9,1,9,4,9,3,9,1001,9,2,9,4,9,3,9,1001,9,2,9,4,9,3,9,102,2,9,9,4,9,3,9,1001,9,2,9,4,9,3,9,1002,9,2,9,4,9,99,3,9,1001,9,1,9,4,9,3,9,1001,9,1,9,4,9,3,9,1002,9,2,9,4,9,3,9,1001,9,1,9,4,9,3,9,1002,9,2,9,4,9,3,9,102,2,9,9,4,9,3,9,1001,9,2,9,4,9,3,9,1001,9,2,9,4,9,3,9,101,2,9,9,4,9,3,9,102,2,9,9,4,9,99"
 
-    #print(instructions)
-    #print(instructions[position], i, mode1, mode2, mode3)
+        instructions = intcode.split(",")
 
-    if(i==1):
-        a = instructions[position+1] if mode1 else instructions[instructions[position+1]]
-        b = instructions[position+2] if mode2 else instructions[instructions[position+2]]
-        
-        instructions[instructions[position+3]] = a+b
+        # convert all numbers to int
+        instructions = [int(x) for x in instructions]
 
-        position += 4
-    elif(i==2):
-        a = instructions[position+1] if mode1 else instructions[instructions[position+1]]
-        b = instructions[position+2] if mode2 else instructions[instructions[position+2]]
+        # current instruction position
+        position = 0
 
-        instructions[instructions[position+3]] = a*b
+        # multiple input instructions per stage
+        inputOccurrence = 0
 
-        position += 4
-    elif(i==3):
-        newPosition = instructions[position+1]
-        # take an input and store it at address given by parameter
-        instructions[newPosition] = inputValue
-        position += 2
-    elif(i==4):
-        # outputs the value of its only parameter
-        outputValue = instructions[position+1] if mode1 else instructions[instructions[position+1]]
-        #print("Output at", position, "is", outputValue)
-        position += 2
-    elif(i==5):
-        # jump if true
-        a = instructions[position+1] if mode1 else instructions[instructions[position+1]]
-        b = instructions[position+2] if mode2 else instructions[instructions[position+2]]
-        if (a != 0):
-            position = b
-        else:
-            position += 3
-    elif(i==6):
-        # jump if false
-        a = instructions[position+1] if mode1 else instructions[instructions[position+1]]
-        b = instructions[position+2] if mode2 else instructions[instructions[position+2]]
-        if (a == 0):
-            position = b
-        else:
-            position += 3
-    elif(i==7):
-        # less than
-        a = instructions[position+1] if mode1 else instructions[instructions[position+1]]
-        b = instructions[position+2] if mode2 else instructions[instructions[position+2]]
+        # end at 99
+        while(instructions[position] != 99):
+            # current op code (lowest 2 digits)
+            i = instructions[position]%100
+            
+            # get mode of each parameter
+            # // does floor division (no floats)
+            # %10 only gets that digit
+            mode1 = (instructions[position]//  100)%10
+            mode2 = (instructions[position]// 1000)%10
+            mode3 = (instructions[position]//10000)%10
 
-        if (mode3):
-            instructions[position+3] = int(a<b)
-        else:
-            instructions[instructions[position+3]] = int(a<b)
+            #print(instructions)
+            #print(instructions[position], i, mode1, mode2, mode3)
 
-        position+=4
-    elif(i==8):
-        # equals
-        a = instructions[position+1] if mode1 else instructions[instructions[position+1]]
-        b = instructions[position+2] if mode2 else instructions[instructions[position+2]]
+            if(i==1):
+                a = instructions[position+1] if mode1 else instructions[instructions[position+1]]
+                b = instructions[position+2] if mode2 else instructions[instructions[position+2]]
+                
+                instructions[instructions[position+3]] = a+b
 
-        if (mode3):
-            instructions[position+3] = int(a==b)
-        else:
-            instructions[instructions[position+3]] = int(a==b)
+                position += 4
+            elif(i==2):
+                a = instructions[position+1] if mode1 else instructions[instructions[position+1]]
+                b = instructions[position+2] if mode2 else instructions[instructions[position+2]]
 
-        position+=4
-    else:
-        print("ERROR")
+                instructions[instructions[position+3]] = a*b
 
-#print(instructions)
-print("Output Value:", outputValue)
+                position += 4
+            elif(i==3):
+                newPosition = instructions[position+1]
+                # take an input and store it at address given by parameter
+                instructions[newPosition] = storage[amplifierStage][inputOccurrence]
+                inputOccurrence+=1
+                position += 2
+            elif(i==4):
+                # outputs the value of its only parameter
+                storage[amplifierStage+1][1] = instructions[position+1] if mode1 else instructions[instructions[position+1]]
+                position += 2
+            elif(i==5):
+                # jump if true
+                a = instructions[position+1] if mode1 else instructions[instructions[position+1]]
+                b = instructions[position+2] if mode2 else instructions[instructions[position+2]]
+                if (a != 0):
+                    position = b
+                else:
+                    position += 3
+            elif(i==6):
+                # jump if false
+                a = instructions[position+1] if mode1 else instructions[instructions[position+1]]
+                b = instructions[position+2] if mode2 else instructions[instructions[position+2]]
+                if (a == 0):
+                    position = b
+                else:
+                    position += 3
+            elif(i==7):
+                # less than
+                a = instructions[position+1] if mode1 else instructions[instructions[position+1]]
+                b = instructions[position+2] if mode2 else instructions[instructions[position+2]]
+
+                if (mode3):
+                    instructions[position+3] = int(a<b)
+                else:
+                    instructions[instructions[position+3]] = int(a<b)
+
+                position+=4
+            elif(i==8):
+                # equals
+                a = instructions[position+1] if mode1 else instructions[instructions[position+1]]
+                b = instructions[position+2] if mode2 else instructions[instructions[position+2]]
+
+                if (mode3):
+                    instructions[position+3] = int(a==b)
+                else:
+                    instructions[instructions[position+3]] = int(a==b)
+
+                position+=4
+            else:
+                print("ERROR")
+
+    # store the highest signal from all of the permutations
+    if (storage[5][1] > highestSignal):
+        highestSignal = storage[5][1]
+
+    #print("Phase Setting:", possibility, "Output Signal:", storage[5][1])
+
+print("Highest Signal:", highestSignal)
