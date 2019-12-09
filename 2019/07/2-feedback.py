275ca2e39300e6dd4bffcb00564747c40dd1b9e1
Dillon Nichols <ee.tinkerer@gmail.com>
2019-12-09 10:24:06 -0500

    only use that amp's instruction pointer

--- 2019/07/2-feedback.py
+++ 2019/07/2-feedback.py
@@ -39,84 +39,84 @@ for i in range(5):
 ip = [0,0,0,0,0]
 
 # end at 99
-while(instructions[amp][ip] != 99):
+while(instructions[amp][ip[amp]] != 99):
     # current op code (lowest 2 digits)
-    i = instructions[amp][ip]%100
+    i = instructions[amp][ip[amp]]%100
     
     # get mode of each parameter
     # // does floor division (no floats)
     # %10 only gets that digit
-    mode1 = (instructions[amp][ip]//  100)%10
-    mode2 = (instructions[amp][ip]// 1000)%10
-    mode3 = (instructions[amp][ip]//10000)%10
+    mode1 = (instructions[amp][ip[amp]]//  100)%10
+    mode2 = (instructions[amp][ip[amp]]// 1000)%10
+    mode3 = (instructions[amp][ip[amp]]//10000)%10
 
     #print(instructions[amp])
-    #print(instructions[amp][ip], i, mode1, mode2, mode3)
+    #print(instructions[amp][ip[amp]], i, mode1, mode2, mode3)
 
     if(i==1):
-        a = instructions[amp][ip+1] if mode1 else instructions[amp][instructions[amp][ip+1]]
-        b = instructions[amp][ip+2] if mode2 else instructions[amp][instructions[amp][ip+2]]
+        a = instructions[amp][ip[amp]+1] if mode1 else instructions[amp][instructions[amp][ip[amp]+1]]
+        b = instructions[amp][ip[amp]+2] if mode2 else instructions[amp][instructions[amp][ip[amp]+2]]
         
-        instructions[amp][instructions[amp][ip+3]] = a+b
+        instructions[amp][instructions[amp][ip[amp]+3]] = a+b
 
-        ip += 4
+        ip[amp] += 4
     elif(i==2):
-        a = instructions[amp][ip+1] if mode1 else instructions[amp][instructions[amp][ip+1]]
-        b = instructions[amp][ip+2] if mode2 else instructions[amp][instructions[amp][ip+2]]
+        a = instructions[amp][ip[amp]+1] if mode1 else instructions[amp][instructions[amp][ip[amp]+1]]
+        b = instructions[amp][ip[amp]+2] if mode2 else instructions[amp][instructions[amp][ip[amp]+2]]
 
-        instructions[amp][instructions[amp][ip+3]] = a*b
+        instructions[amp][instructions[amp][ip[amp]+3]] = a*b
 
-        ip += 4
+        ip[amp] += 4
     elif(i==3):
-        newPosition = instructions[amp][ip+1]
+        newPosition = instructions[amp][ip[amp]+1]
         # take an input and store it at address given by parameter
         # get the last element
         instructions[amp][newPosition] = storage[amp][-1]
-        ip += 2
+        ip[amp] += 2
     elif(i==4):
         # outputs the value of its only parameter
         # store the output to the next amp (using modulo to feed back the last amp to the first)
         # append it to the end
-        storage[(amp+1)%5].append(instructions[amp][ip+1] if mode1 else instructions[amp][instructions[amp][ip+1]])
-        ip += 2
+        storage[(amp+1)%5].append(instructions[amp][ip[amp]+1] if mode1 else instructions[amp][instructions[amp][ip[amp]+1]])
+        ip[amp] += 2
     elif(i==5):
         # jump if true
-        a = instructions[amp][ip+1] if mode1 else instructions[amp][instructions[amp][ip+1]]
-        b = instructions[amp][ip+2] if mode2 else instructions[amp][instructions[amp][ip+2]]
+        a = instructions[amp][ip[amp]+1] if mode1 else instructions[amp][instructions[amp][ip[amp]+1]]
+        b = instructions[amp][ip[amp]+2] if mode2 else instructions[amp][instructions[amp][ip[amp]+2]]
         if (a != 0):
-            ip = b
+            ip[amp] = b
         else:
-            ip += 3
+            ip[amp] += 3
     elif(i==6):
         # jump if false
-        a = instructions[amp][ip+1] if mode1 else instructions[amp][instructions[amp][ip+1]]
-        b = instructions[amp][ip+2] if mode2 else instructions[amp][instructions[amp][ip+2]]
+        a = instructions[amp][ip[amp]+1] if mode1 else instructions[amp][instructions[amp][ip[amp]+1]]
+        b = instructions[amp][ip[amp]+2] if mode2 else instructions[amp][instructions[amp][ip[amp]+2]]
         if (a == 0):
-            ip = b
+            ip[amp] = b
         else:
-            ip += 3
+            ip[amp] += 3
     elif(i==7):
         # less than
-        a = instructions[amp][ip+1] if mode1 else instructions[amp][instructions[amp][ip+1]]
-        b = instructions[amp][ip+2] if mode2 else instructions[amp][instructions[amp][ip+2]]
+        a = instructions[amp][ip[amp]+1] if mode1 else instructions[amp][instructions[amp][ip[amp]+1]]
+        b = instructions[amp][ip[amp]+2] if mode2 else instructions[amp][instructions[amp][ip[amp]+2]]
 
         if (mode3):
-            instructions[amp][ip+3] = int(a<b)
+            instructions[amp][ip[amp]+3] = int(a<b)
         else:
-            instructions[amp][instructions[amp][ip+3]] = int(a<b)
+            instructions[amp][instructions[amp][ip[amp]+3]] = int(a<b)
 
-        ip+=4
+        ip[amp]+=4
     elif(i==8):
         # equals
-        a = instructions[amp][ip+1] if mode1 else instructions[amp][instructions[amp][ip+1]]
-        b = instructions[amp][ip+2] if mode2 else instructions[amp][instructions[amp][ip+2]]
+        a = instructions[amp][ip[amp]+1] if mode1 else instructions[amp][instructions[amp][ip[amp]+1]]
+        b = instructions[amp][ip[amp]+2] if mode2 else instructions[amp][instructions[amp][ip[amp]+2]]
 
         if (mode3):
-            instructions[amp][ip+3] = int(a==b)
+            instructions[amp][ip[amp]+3] = int(a==b)
         else:
-            instructions[amp][instructions[amp][ip+3]] = int(a==b)
+            instructions[amp][instructions[amp][ip[amp]+3]] = int(a==b)
 
-        ip+=4
+        ip[amp]+=4
     else:
         print("ERROR")
 
