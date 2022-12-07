c44df169e669ea4d0d26d733c138a0b4e234af94
Dillon Nichols <ee.tinkerer@gmail.com>
2022-12-07 14:52:28 -0500

    have it working with the sample data

--- 2022/05/1-crates.py
+++ 2022/05/1-crates.py
@@ -43,13 +43,35 @@ for t in range(len(temp)):
                 crates[i+1].append(temp[t][i])
 
 
-# now a dict with the column # as key, and crates as arrays
+# now a dict with the column # as key, and stacks as arrays
 print(crates)
 
 
 
+# loop through instructions
+for l, line in enumerate(txt):
+    # skip the picture
+    if (l > pic_h):
+        # instruction
+        ins = line.strip('\n').split(' ')
+        # number of crates to move
+        num = int(ins[1])
+        # source
+        s = int(ins[3])
+        # destination
+        d = int(ins[5])
+        
+        #print('\n', num, s, d)
 
-# for line in txt:
-#     # between the picture and the instructions is a newline
-#     if (line == '\n'):
-#         break
+        # crates are moved one at a time
+        for n in range(num):
+            #print(n)
+            # copy last item of source to destination
+            crates[d].append(crates[s][-1])
+            # remove last item of source
+            crates[s].pop(-1)
+
+        #print(crates)
+
+for x in range(1,4):
+    print(crates[x][-1],)
